73f5bafdf1002278ff6bdf5ac5a88cb0ac444ac8
Karaby <fablelite11@gmail.com>
2024-06-03 23:00:10 +0300

    Сделал домашку

--- src/main/java/ru/skypro/homework29/controller/EmployeeController.java
+++ src/main/java/ru/skypro/homework29/controller/EmployeeController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/employees")
+@RequestMapping("/departments")
 public class EmployeeController {
 
     private final EmployeeService employeeService;
