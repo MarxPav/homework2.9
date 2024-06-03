1fda0805217c6f31d2c2850cbb847d546b40ac60
Karaby <fablelite11@gmail.com>
2024-06-03 23:00:36 +0300

    Сделал домашку

--- src/main/java/ru/skypro/homework29/service/EmployeeService.java
+++ src/main/java/ru/skypro/homework29/service/EmployeeService.java
@@ -10,7 +10,7 @@ public interface EmployeeService {
 
     Employee getEmployeeWithMaxSalary(int department);
 
-     List<Employee> getAllEmployees(int department);
+    List<Employee> getAllEmployees(int department);
 
-     Map<Integer, List<Employee>> getAllEmployees();
+    Map<Integer, List<Employee>> getAllEmployeesDepartment();
 }
